71803f5205dde347333b1390362a342d429d473d
iita <iita@helsinki.fi>
2020-11-24 22:29:38 +0200

    testejä lisää

--- Ristinolla/src/test/java/GameTest.java
+++ Ristinolla/src/test/java/GameTest.java
@@ -29,7 +29,7 @@ public class GameTest {
     public void tearDown() {
     }
 
-        @Test
+    @Test
     public void XHasFirstTurn() {
         assertEquals("X", logic.getTurn());
     }
@@ -38,5 +38,48 @@ public class GameTest {
     public void SecondTurnIs0() {
         logic.changeTurn();
         assertEquals("0", logic.getTurn());
+    }
+        @Test
+    public void DuringFirstTurnScoreboardSetsX() {
+        logic.updateScore(0, 0);
+        assertEquals("X", logic.getSymbolFromScoreBoard(0, 0));
+    }
+
+    @Test
+    public void DuringTheSecondTurnScoreboardSets0() {
+        logic.changeTurn();
+        logic.updateScore(0, 0);
+        assertEquals("0", logic.getSymbolFromScoreBoard(0, 0));
+    }
+    @Test
+    public void GameIsNotEndingEarlyColumns() {
+        logic.updateScore(0, 0);
+        logic.updateScore(1, 2);
+        logic.updateScore(2, 0);
+        assertEquals(false, logic.isThereAWinnerInColumns(2, 0));
+    }
+
+    @Test
+    public void GameIsNotEndingEarlyInRows() {
+        logic.updateScore(0, 0);
+        logic.updateScore(1, 2);
+        logic.updateScore(0, 1);
+        assertEquals(false, logic.isThereAWinnerInRows(0, 1));
+    }
+    
+    @Test
+    public void GamesEndsWhenSymbolsAreInTheRow() {
+        logic.updateScore(0, 0);
+        logic.updateScore(0, 1);
+        logic.updateScore(0, 2);
+        assertEquals(true, logic.isThereAWinnerInRows(0, 2));
+    }
+
+    @Test
+    public void GamesEndsWhenSymbolsAreinTheColumn() {
+        logic.updateScore(0, 0);
+        logic.updateScore(1, 0);
+        logic.updateScore(2, 0);
+        assertEquals(true, logic.isThereAWinnerInColumns(2, 0));
     }
 }
